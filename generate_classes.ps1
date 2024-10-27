939961ad354d012cd99071cc2e0f0b4f8346ff49
Deft Developer <dev@hymes.name>
2024-10-27 14:42:59 -0700

    Throws exception and stops if a python generator exits non-zero.

--- generate_classes.ps1
+++ generate_classes.ps1
@@ -1,5 +1,21 @@
 #!/usr/bin/env pwsh
-
+# MIT License
+# Copyright (c) 2024 Charles Hymes
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 # This powershell script is NOT for any release, and has no end-user value.
 #
@@ -18,7 +34,7 @@ $WORKFLOWS_ALL = @(
     "comfyui_default",
     "flux_1.0",
     "flux_neg_1.1",
-    "flux_neg_upscale_sdxl_0.4",
+    "flux_neg_upscale_sdxl_0.5",
     "img2img_sdxl_0.3",
     "inpainting_sdxl_0.4",
     "sytan_sdxl_1.0"
@@ -59,7 +75,12 @@ function Get-APIJsonsMap([string[]]$SubjectWorkflows) {
 function Generate-Classes([String] $generator, [string[]]$SubjectWorkflows) {
     $wfmap = Get-APIJsonsMap($SubjectWorkflows)
     foreach ($workflow in $SubjectWorkflows) {
-        python $generator $wfmap[$workflow]
+        $json = $wfmap[$workflow]
+        python $generator $json
+        $failed = $LastExitCode -ne 0
+        if ($failed){
+            throw "Error running generator `"$generator`" on `"$json`""
+        }
     }
 }
 
@@ -88,8 +109,6 @@ foreach ($generator_name in $GENERATORS) {
     $generator = Join-Path -Path $PSScriptRoot -ChildPath "workflow" -AdditionalChildPath @($generator_name)
     Write-Information "Running generator `"$generator`"" -InformationAction Continue
     Generate-Classes $generator $WORKFLOWS_ALL
-    #   Generate-Classes $generator @("sytan_sdxl_1.0")
-    #   Generate-Classes $generator $WORKFLOWS_FLUX
 }
 
 # Test with
